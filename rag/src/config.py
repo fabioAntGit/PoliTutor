971b9cab08eea96946ab4260d9b14af4a6e599bb
José Barbosa <150469403+JosePedroBarbosa@users.noreply.github.com>
2026-02-27 17:55:02 +0000

    Suporte ficheiros pdf, pptx e md
    Adicionado suporte a processamento de ficheiros pdf, pptx e md. Os tipos de ficheiros suportados podem ser alterados na config futuramente. No futuro deve ser alterado também o nome do ficheiro pdf_extractor para extractor apenas para generalizar.

--- rag/src/config.py
+++ rag/src/config.py
@@ -33,18 +33,22 @@ ELEMENT_TYPES_TO_EXCLUDE = [
     "UncategorizedText"
 ]
 
+# --- File Discovery ---
+SUPPORTED_EXTENSIONS = ["*.pdf", "*.pptx", "*.md"]
+
 # --- Unstructured Partitioning Configuration ---
-PDF_PROCESSING_CONFIG = {
+FILE_PROCESSING_CONFIG = {
     "strategy": "hi_res",
     "languages": ["por", "eng"],
     "infer_table_structure": True,
     "extract_image_block_types": ["Image"],
     "extract_image_block_to_payload": True,
     "chunking_strategy": None, 
+    "skip_infer_table_types": ["md"],
 }
 
 # --- Chroma DB Collection
-CHROMA_COLLECTION_NAME = "PoliTutor-Docs"
+CHROMA_COLLECTION_NAME = "PoliTutor-Docs-TESTFILES"
 
 # --- ChromaDB HNSW Index ---
 CHROMA_HNSW_SPACE = "cosine"
