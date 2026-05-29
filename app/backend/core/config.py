5336573171041288543ed1c526a1a8c7df97d604
Fábio Antunes <107940399+fabioAntGit@users.noreply.github.com>
2026-05-29 20:06:01 +0100

    fix error

--- app/backend/core/config.py
+++ app/backend/core/config.py
@@ -5,13 +5,13 @@ from pathlib import Path
 
 load_dotenv(Path(__file__).parent.parent / ".env")
 
-MONGO_URI = os.environ["MONGODB_URI"]
+MONGO_URI = os.getenv("MONGODB_URI", "")
 MONGO_DB = os.getenv("MONGODB_DB", "poli_tutor")
 
 REDIS_HOST = os.getenv("REDIS_HOST", "redis")
 REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
 REDIS_TTL = int(os.getenv("REDIS_TTL", 86400))
 
-JWT_SECRET_KEY = os.environ["JWT_SECRET_KEY"]
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "")
 JWT_ALGORITHM = os.getenv("JWT_ALGORITHM", "HS256")
 JWT_ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("JWT_ACCESS_TOKEN_EXPIRE_MINUTES", "480"))
